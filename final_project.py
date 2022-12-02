590949e51692d817afe7cc911ba3a3682abf590f
RageF1ghter <42597634+RageF1ghter@users.noreply.github.com>
2022-12-01 22:04:56 -0700

    Add files via upload

--- final_project.py
+++ final_project.py
@@ -1,9 +1,285 @@
 from tkinter import *
 import psycopg2
 from datetime import timedelta, date
+from tkinter import ttk
 
 # global var: member_id1, username1, global bookName, bookIsbn, bookAuthor, bookGenre
+# member_id2, bookIsbn2
 
+# 0743477103
+# 1 2
+
+
+# admin part
+def deleteBooks():
+    #set query
+    conn = psycopg2.connect(dbname = 'libraryManager', user="postgres", password="199814", host="127.0.0.1", port="5432")
+    cur = conn.cursor()
+
+    def deleteBook():
+        cur.execute(f"select count(copy_number) as count from copy where isbn = '{deleteIsbn.get()}'")
+        for count in cur:
+            if count[0] == 0:
+                cur.execute(f"DELETE FROM books WHERE isbn = '{deleteIsbn.get()}'")
+                conn.commit()
+                Label(root, text="This book has been deleted").place(x=100, y=120)
+            else:
+                Label(root, text="Still have copies").place(x=100, y=120)
+        
+    def deleteCopy():
+        cur.execute(f"select availability from copy where isbn = '{deleteIsbn.get()}' and copy_number = {deleteCopyNumber.get()}")
+        for availability in cur:
+            if availability[0] == True:
+                cur.execute(f"DELETE FROM copy WHERE isbn = '{deleteIsbn.get()}' and copy_number = {deleteCopyNumber.get()}")
+                conn.commit()
+                Label(root, text="This copy has been deleted").place(x=100, y=230)
+            else:
+                Label(root, text="This copy is borrowed by the member").place(x=100, y=230)
+
+    #set frame
+    root=Toplevel(screen)
+    root.geometry('300x300')
+
+    #set delete book
+    lable1 = Label(root, text='enter book isbn')
+    lable1.place(x=70, y=50)
+    deleteIsbn = StringVar()
+    Entry(root, textvariable=deleteIsbn).place(x=200, y=50)
+    Button(root, text="Delete book", command=deleteBook).place(x=100, y=100)
+
+    #set delete copy
+    deleteCopyNumber = IntVar()
+    Label(root, text='enter copy number').place(x=70, y=150)
+    Entry(root, textvariable=deleteCopyNumber).place(x=200, y=150)
+    Button(root, text="Delete copy", command=deleteCopy).place(x=100, y=200)
+
+def addBooks():
+    #set query
+    conn = psycopg2.connect(dbname = 'libraryManager', user="postgres", password="199814", host="127.0.0.1", port="5432")
+    cur = conn.cursor()
+
+    def add():
+        #insert book
+        cur.execute(f"insert into books(title, isbn, author, publisher, genre) values('{newBookName.get()}', '{newBookIsbn.get()}', '{newBookGenre.get()}', '{newBookAuthor.get()}', '{newBookPublisher.get()}')")
+        print("book added")
+        conn.commit()
+
+    def addCopies():
+        for index in range(numberOfCopies.get()):
+            cur.execute(f"insert into copy(copy_number, availability, isbn) values ({index+1}, true, '{newBookIsbn.get()}')")
+            print(f'{index+1} add successfully')
+        conn.commit()
+        
+    #set frame
+    root=Toplevel(screen)
+    root.geometry('800x600')
+
+    #set label
+    lable1 = Label(root, text='enter book title')
+    lable1.place(x=70, y=50)
+    lable2 = Label(root, text='enter book isbn')
+    lable2.place(x=70, y=100)
+    lable3 = Label(root, text='enter book genre')
+    lable3.place(x=70, y=150)
+    lable4 = Label(root, text='enter book author')
+    lable4.place(x=70, y=200)
+    lable5 = Label(root, text='enter book Publisher')
+    lable5.place(x=70, y=250)
+    
+    #set textfiled
+    newBookName = StringVar()
+    newBookIsbn = StringVar()
+    newBookGenre = StringVar()
+    newBookAuthor = StringVar()
+    newBookPublisher = StringVar()
+    bookNameInput = Entry(root, textvariable=newBookName, width=20)
+    bookNameInput.place(x=200, y=50)
+    bookIsbnInput = Entry(root, textvariable=newBookIsbn, width=20)
+    bookIsbnInput.place(x=200, y=100)
+    bookGenreInput = Entry(root, textvariable=newBookGenre, width=20)
+    bookGenreInput.place(x=200, y=150)
+    bookAuthorInput = Entry(root, textvariable=newBookAuthor, width=20)
+    bookAuthorInput.place(x=200, y=200)
+    bookAuthorInput = Entry(root, textvariable=newBookPublisher, width=20)
+    bookAuthorInput.place(x=200, y=250)
+    Button(root, text="Add Book", command=add).place(x=100, y=300)
+
+    lable6 = Label(root, text='enter number of copies')
+    lable6.place(x=70, y=350)
+    numberOfCopies = IntVar()
+    Entry(root, textvariable=numberOfCopies, width=20).place(x=230, y=350)
+    Button(root, text="Add Copy", command=addCopies).place(x=100, y=400)
+
+def manageBookByIsbn():
+    #set query
+    conn = psycopg2.connect(dbname = 'libraryManager', user="postgres", password="199814", host="127.0.0.1", port="5432")
+    cur = conn.cursor()
+
+    #set frame
+    root=Toplevel(screen)
+    root.geometry('800x600')
+
+    cur.execute(f"select books.title as title, copy.copy_number as copy_number, copy.availability as availability, borrowed.member_id as member_id, member.name as name from books, copy, borrowed, member where books.isbn = copy.isbn and copy.isbn = borrowed.isbn and copy.copy_number = borrowed.copy_number and borrowed.member_id = member.member_id and books.isbn = '{bookIsbn2.get()}'")
+    index = 0
+    for title, copy_number1, availability, member_id, name in cur:
+        if availability == True:
+            Label(root, text=f"{title}(copy: {copy_number1}) has been borrowed by {name}(member id: {member_id}), and has been returned").grid(row=index, column=0)
+            index += 1
+        else:
+            Label(root, text=f"{title}(copy: {copy_number1}) has been borrowed by {name}(member id: {member_id}), and hasn't been returned").grid(row=index, column=0)
+            index += 1
+
+def findMemberById():
+    #set query
+    conn = psycopg2.connect(dbname = 'libraryManager', user="postgres", password="199814", host="127.0.0.1", port="5432")
+    cur = conn.cursor()
+
+    #set the frame and scrollbar
+    root=Toplevel(screen)
+    root.geometry('600x600')
+    frame = Frame(root)
+    frame.pack(fill=BOTH, expand=1)
+    canvas = Canvas(frame)
+    canvas.pack(side=LEFT, fill=BOTH, expand=1)
+    scrollbar = ttk.Scrollbar(frame, orient=VERTICAL, command=canvas.yview)
+    scrollbar.pack(side=RIGHT, fill=Y)
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+    second_frame = Frame(canvas)
+    canvas.create_window((0,0), window=second_frame, anchor="nw")
+
+
+    def returnBook(isbn, copy_number):
+        #update the borrowed table
+        cur.execute(f"UPDATE copy SET availability = True WHERE isbn = '{isbn}' and copy_number = {copy_number}")
+        #update the returned table
+        returnDate = date.today()
+        cur.execute(f"insert into returned(return_date, copy_number, isbn, member_id) values ('{returnDate}', {copy_number}, '{isbn}', {member_id2.get()})")
+        conn.commit()
+        print("succeesfully returned")
+        
+
+    # set labels
+    cur.execute(f"select copy.isbn as isbn, copy.copy_number as copy_number, borrowed.due_date as due, member.member_id as member_id, member.name as name from copy, borrowed, member where copy.isbn = borrowed.isbn and copy.availability=False and borrowed.member_id = member.member_id and member.member_id = {member_id2.get()}")
+    rowNum = 0
+    tempCopyNumber = 0
+    tempDueDate = date(2010, 2, 13)
+    for isbn, copy_number, due, member_id, name in cur:
+        if tempCopyNumber != copy_number:
+            tempCopyNumber = copy_number
+            if rowNum == 0:
+                Label(second_frame, text=f'{name} member id: {member_id} borrowed {isbn} (copy {copy_number}) which will due on {due}').grid(row=rowNum, column=0)
+                rowNum+=1
+                Button(second_frame, text="mark as return", command=lambda: returnBook(isbn=isbn, copy_number=copy_number)).grid(row=rowNum, column=0)
+                rowNum+=1
+            if tempDueDate != date(2010, 2, 13):
+                Label(second_frame, text=f'{name} member id: {member_id} borrowed {isbn} (copy {copy_number}) which will due on {due}').grid(row=rowNum, column=0)
+                rowNum+=1
+                Button(second_frame, text="mark as return", command=lambda: returnBook(isbn=isbn, copy_number=copy_number)).grid(row=rowNum, column=0)
+                rowNum+=1    
+        else:
+            if due>tempDueDate:
+                tempDueDate = due
+
+def checkMembers():
+    #set query
+    conn = psycopg2.connect(dbname = 'libraryManager', user="postgres", password="199814", host="127.0.0.1", port="5432")
+    cur = conn.cursor()
+
+    #set the frame and scrollbar
+    root=Toplevel(screen)
+    root.geometry('600x600')
+    frame = Frame(root)
+    frame.pack(fill=BOTH, expand=1)
+    canvas = Canvas(frame)
+    canvas.pack(side=LEFT, fill=BOTH, expand=1)
+    scrollbar = ttk.Scrollbar(frame, orient=VERTICAL, command=canvas.yview)
+    scrollbar.pack(side=RIGHT, fill=Y)
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+    second_frame = Frame(canvas)
+    canvas.create_window((0,0), window=second_frame, anchor="nw")
+
+    # set labels
+    cur.execute("select borrowed.copy_number as copy_number, borrowed.due_date as due_date, member.member_id as member_id, member.name as name, books.title as title from member, borrowed, books where member.member_id = borrowed.member_id and books.isbn = borrowed.isbn")
+    rowNum = 0
+    for copy_number, due_date, member_id, name, title in cur:
+        Label(second_frame, text=f'{name} member id: {member_id} borrowed {title} (copy {copy_number}) which dues on {due_date}').grid(row=rowNum, column=0)
+        rowNum+=1
+
+def checkBooks():
+    #set query
+    conn = psycopg2.connect(dbname = 'libraryManager', user="postgres", password="199814", host="127.0.0.1", port="5432")
+    cur = conn.cursor()
+
+    #set the frame and scrollbar
+    root=Toplevel(screen)
+    root.geometry('600x600')
+    frame = Frame(root)
+    frame.pack(fill=BOTH, expand=1)
+    canvas = Canvas(frame)
+    canvas.pack(side=LEFT, fill=BOTH, expand=1)
+    scrollbar = ttk.Scrollbar(frame, orient=VERTICAL, command=canvas.yview)
+    scrollbar.pack(side=RIGHT, fill=Y)
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+    second_frame = Frame(canvas)
+    canvas.create_window((0,0), window=second_frame, anchor="nw")
+
+    # set labels
+    cur.execute("select books.title as title, books.isbn as isbn, copy.copy_number as copy_number, copy.availability as availability from books, copy where books.isbn = copy.isbn")
+    rowNum = 0
+    for title, isbn, copy_number, availability in cur:
+        Label(second_frame, text=f'{title} {isbn} copy number: {copy_number} availability: {availability}').grid(row=rowNum, column=0)
+        rowNum+=1
+
+# member part
+def memberReturn():
+    #set query
+    conn = psycopg2.connect(dbname = 'libraryManager', user="postgres", password="199814", host="127.0.0.1", port="5432")
+    cur = conn.cursor()
+
+    
+    def returnBook(isbn, copy_number):
+        returnIsbnStr = isbn.get()
+        copyNumStr = copy_number.get()
+        #update the borrowed table
+        cur.execute(f"UPDATE copy SET availability = True WHERE isbn = '{returnIsbnStr}' and copy_number = {copyNumStr}")
+        #update the returned table
+        returnDate = date.today()
+        cur.execute(f"insert into returned(return_date, copy_number, isbn, member_id) values ('{returnDate}', {copyNumStr}, '{returnIsbnStr}', {member_id1})")
+        conn.commit()
+        print("succeesfully returned")
+        
+
+    #set frame
+    root=Toplevel(screen)
+    root.geometry('600x600')
+    #
+    # 9780358380245
+    # 1
+    #
+    #set label
+    lable1 = Label(root, text="Enter the book's isbn you want to return: ")
+    lable1.place(x=50, y=100)
+    lable2 = Label(root, text="Enter the book's borrow date you want to return: ")
+    lable2.place(x=50, y=150)
+    lable3 = Label(root, text="Enter the book's copy number you want to return: ")
+    lable3.place(x=50, y=200)
+    #set textfield
+    returnIsbn = StringVar()
+    borrowDate = StringVar()
+    copyNum = StringVar()
+    isbnInput = Entry(root, textvariable=returnIsbn, width=12)
+    isbnInput.place(x=300, y=100)
+    dateInput = Entry(root,textvariable=borrowDate, width=12)
+    dateInput.place(x=300, y=150)
+    copyInput = Entry(root,textvariable=copyNum, width=12)
+    copyInput.place(x=300, y=200)
+    #set button
+    button = Button(root, text="return",padx=50, command= lambda: returnBook(isbn=returnIsbn, copy_number=copyNum))
+    button.place(x=200, y=250)
+    
 def findBookByAuthor():
     labels = []
     #set query
@@ -67,7 +343,6 @@ def findBookByGenre():
             labels.append(Label(root, text='------------------------'))
             labels[index].place(x=100, y=130+index*30)
             index += 1
-
 
 def findBookByIsbn():
     #set query
@@ -167,8 +442,7 @@ def findBookByTitle():
             
     #set buttons
     button1 = Button(root, text="Borrow",padx=50, command=lambda: borrowBook(isbn))
-    button1.place(x=100, y=300)       
-    
+    button1.place(x=100, y=300)           
 
 def memberSearch():
     global bookName
@@ -211,10 +485,11 @@ def memberSearch():
     button1 = Button(root, text="search",padx=50, command=findBookByAuthor)
     button1.place(x=100, y=225)
 
-
-
 def selectionScreen():
-    global member_id1
+    global member_id1   #used for member sites
+    global member_id2   #used for admin sites
+    global bookIsbn2    #used for admin sites
+
     conn = psycopg2.connect(dbname = 'libraryManager', user="postgres", password="199814", host="127.0.0.1", port="5432")
     cur = conn.cursor()
 
@@ -223,12 +498,38 @@ def selectionScreen():
         if username1.get() == username and password1.get() == password:
             root=Toplevel(screen)
             root.geometry('500x500')
-            lable0 = Label(root, text='admin page')
-            lable0.place(x=30, y=50)
-            lable1 = Label(root, text=username1.get())
-            lable1.place(x=70, y=100)
-            lable2 = Label(root, text=password1.get())
-            lable2.place(x=70, y=150)
+            label0 = Label(root, text='admin page')
+            label0.place(x=30, y=50)
+
+            button2 = Button(root, text="check books", padx=50, command=checkBooks)
+            button2.place(x=70, y=100)
+
+            button3 = Button(root, text="check members", padx=50, command=checkMembers)
+            button3.place(x=70, y=150)
+
+            label1 = Label(root, text="Search member by id:")
+            label1.place(x=70, y=200)
+            member_id2=StringVar()
+            memberIDInput = Entry(root, textvariable=member_id2, width=12)
+            memberIDInput.place(x=250, y=200)
+            button4 = Button(root, text="Search", padx=50, command=findMemberById)
+            button4.place(x=70, y=250)
+            
+
+            label2 = Label(root, text="Search book by isbn:")
+            label2.place(x=70, y=300)
+            bookIsbn2=StringVar()
+            ISBNInput = Entry(root, textvariable=bookIsbn2, width=12)
+            ISBNInput.place(x=250, y=300)
+            button5 = Button(root, text="Search", padx=50, command=manageBookByIsbn)
+            button5.place(x=70, y=350)
+
+            button6 = Button(root, text="Add books and copys", padx=50, command=addBooks)
+            button6.place(x=70, y=400)
+
+            button6 = Button(root, text="Delete books", padx=50, command=deleteBooks)
+            button6.place(x=70, y=450)
+
             break
 
     cur.execute('SELECT username, password, member_id mem FROM member_login')
@@ -238,11 +539,10 @@ def selectionScreen():
             root.geometry('500x500')
             button1 = Button(root, text="search book",padx=50, command=memberSearch)
             button1.place(x=70, y=100)
-            button1 = Button(root, text="return book",padx=50)
+            button1 = Button(root, text="return book",padx=50, command=memberReturn)
             button1.place(x=70, y=150)
             member_id1 = member_id
             break
-            
 
 def loginScreen():
     global username1
@@ -272,7 +572,5 @@ def loginScreen():
 
 
     screen.mainloop()
-
-
 
 loginScreen()
